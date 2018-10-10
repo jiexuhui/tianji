91b62737ad5d33254c4af7879841ec2a9d0f3ba2
½ÒÐñ»Ô <858152627@qq.com>
2018-10-10 18:29:53 +0800

    修改图片上传地址

--- src/components/Tinymce/components/editorImage.vue
+++ src/components/Tinymce/components/editorImage.vue
@@ -3,7 +3,7 @@
     <el-button icon='el-icon-upload' size="mini" :style="{background:color,borderColor:color}" @click=" dialogVisible=true" type="primary">上传图片
     </el-button>
     <el-dialog append-to-body :visible.sync="dialogVisible">
-      <el-upload class="editor-slide-upload" action="https://httpbin.org/post" :multiple="true" :file-list="fileList" :show-file-list="true"
+      <el-upload class="editor-slide-upload" action="/admin/article/upload" :multiple="true" :file-list="fileList" :show-file-list="true"
         list-type="picture-card" :on-remove="handleRemove" :on-success="handleSuccess" :before-upload="beforeUpload">
         <el-button size="small" type="primary">点击上传</el-button>
       </el-upload>
@@ -17,11 +17,11 @@
 // import { getToken } from 'api/qiniu'
 
 export default {
-  name: 'editorSlideUpload',
+  name: "editorSlideUpload",
   props: {
     color: {
       type: String,
-      default: '#1890ff'
+      default: "#1890ff"
     }
   },
   data() {
@@ -29,61 +29,72 @@ export default {
       dialogVisible: false,
       listObj: {},
       fileList: []
-    }
+    };
   },
   methods: {
     checkAllSuccess() {
-      return Object.keys(this.listObj).every(item => this.listObj[item].hasSuccess)
+      return Object.keys(this.listObj).every(
+        item => this.listObj[item].hasSuccess
+      );
     },
     handleSubmit() {
-      const arr = Object.keys(this.listObj).map(v => this.listObj[v])
+      const arr = Object.keys(this.listObj).map(v => this.listObj[v]);
       if (!this.checkAllSuccess()) {
-        this.$message('请等待所有图片上传成功 或 出现了网络问题，请刷新页面重新上传！')
-        return
+        this.$message(
+          "请等待所有图片上传成功 或 出现了网络问题，请刷新页面重新上传！"
+        );
+        return;
       }
-      console.log(arr)
-      this.$emit('successCBK', arr)
-      this.listObj = {}
-      this.fileList = []
-      this.dialogVisible = false
+      console.log(arr);
+      this.$emit("successCBK", arr);
+      this.listObj = {};
+      this.fileList = [];
+      this.dialogVisible = false;
     },
     handleSuccess(response, file) {
-      const uid = file.uid
-      const objKeyArr = Object.keys(this.listObj)
+      const uid = file.uid;
+      console.log("file>>", file);
+      console.log("response>>", response);
+      const objKeyArr = Object.keys(this.listObj);
       for (let i = 0, len = objKeyArr.length; i < len; i++) {
         if (this.listObj[objKeyArr[i]].uid === uid) {
-          this.listObj[objKeyArr[i]].url = response.files.file
-          this.listObj[objKeyArr[i]].hasSuccess = true
-          return
+          this.listObj[objKeyArr[i]].url = response.data.url;
+          this.listObj[objKeyArr[i]].hasSuccess = true;
+          return;
         }
       }
     },
     handleRemove(file) {
-      const uid = file.uid
-      const objKeyArr = Object.keys(this.listObj)
+      const uid = file.uid;
+      const objKeyArr = Object.keys(this.listObj);
       for (let i = 0, len = objKeyArr.length; i < len; i++) {
         if (this.listObj[objKeyArr[i]].uid === uid) {
-          delete this.listObj[objKeyArr[i]]
-          return
+          delete this.listObj[objKeyArr[i]];
+          return;
         }
       }
     },
     beforeUpload(file) {
-      const _self = this
-      const _URL = window.URL || window.webkitURL
-      const fileName = file.uid
-      this.listObj[fileName] = {}
+      const _self = this;
+      const _URL = window.URL || window.webkitURL;
+      const fileName = file.uid;
+      this.listObj[fileName] = {};
       return new Promise((resolve, reject) => {
-        const img = new Image()
-        img.src = _URL.createObjectURL(file)
+        const img = new Image();
+        img.src = _URL.createObjectURL(file);
         img.onload = function() {
-          _self.listObj[fileName] = { hasSuccess: false, uid: file.uid, width: this.width, height: this.height }
-        }
-        resolve(true)
-      })
+          _self.listObj[fileName] = {
+            hasSuccess: false,
+            uid: file.uid,
+            width: this.width,
+            height: this.height
+          };
+        };
+        resolve(true);
+      });
     }
   }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
--- src/utils/request.js
+++ src/utils/request.js
@@ -21,6 +21,7 @@ service.interceptors.request.use(
       // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
       config.headers.Authorization = getToken();
     }
+    config.headers["Access-Control-Allow-Origin"] = "*";
     config.withCredentials = true;
     return config;
   },
